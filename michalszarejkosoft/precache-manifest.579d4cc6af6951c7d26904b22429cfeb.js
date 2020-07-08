cfa79b04a245ae017609412231b761ce94afa7fc
michalszarejko <37743267+michalszarejko@users.noreply.github.com>
2020-07-08 13:34:40 +0200

    initial commit

---
+++ michalszarejkosoft/precache-manifest.579d4cc6af6951c7d26904b22429cfeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065665cfc1ca23801f7d83f55c861c02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d3a0f8eea569de10cc7",
+    "url": "/static/css/main.eb8cecb8.chunk.css"
+  },
+  {
+    "revision": "e7441efaa3c22100992f",
+    "url": "/static/js/2.d871c498.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d871c498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d3a0f8eea569de10cc7",
+    "url": "/static/js/main.cedc0ccd.chunk.js"
+  },
+  {
+    "revision": "a9fa92489898d91288c6",
+    "url": "/static/js/runtime-main.2f60ff1a.js"
+  },
+  {
+    "revision": "c597fedeefe7868b74c449339c35bc2c",
+    "url": "/static/media/background.c597fede.jpg"
+  }
+]);
